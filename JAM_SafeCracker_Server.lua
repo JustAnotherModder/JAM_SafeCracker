d996b929f9faae00b9d7b06bac6d06a1de68be83
JustAnotherModder <48855422+JustAnotherModder@users.noreply.github.com>
2019-05-07 12:20:41 +0930

    Add files via upload

---
+++ JAM_SafeCracker_Server.lua
@@ -0,0 +1,21 @@
+local JSC = JAM.SafeCracker
+
+function JSC:AddReward(rewards)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	if not xPlayer then return; end
+
+	if rewards.CashAmount then cashReward = xPlayer.addMoney(rewards.CashAmount); end
+
+	for k,v in pairs(rewards.Items) do
+		local randomCount = math.random(1, rewards.DrugsAmount)
+		xPlayer.addInventoryItem(v, randomCount)
+	end
+
+	for i = 1, rewards.WeaponAmount do
+		local randomWeapon = rewards.Weapons[math.random(1, #rewards.Weapons)]
+		xPlayer.addWeapon(randomWeapon, math.random(50, 250))
+	end
+end
+
+RegisterNetEvent('JSC:AddReward')
+AddEventHandler('JSC:AddReward', function(rewards) JSC:AddReward(rewards); end)
